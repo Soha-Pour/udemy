e03ccd56682ffb2b47f2f36fee8ae942a3f9d9fb
Soha Pourpirali <spourpirali@newrelic.com>
2021-10-11 08:29:51 -0700

    overview of the types of variables available in r: integer, double, complex, character, and logical

---
+++ L1_CoreProgrammingPrinciples.R
@@ -0,0 +1,19 @@
+#integer (the "L" makes the value an integer. Without it, R stores the variable as "double.")
+x <- 2L
+typeof(x)
+
+#double 
+y <- 2.5
+typeof(y)
+
+#complex
+z <- 3 + 2i 
+typeof(z)
+
+#character
+a <- "h"
+typeof(a)
+
+#logical 
+q <- TRUE
+typeof(q)
