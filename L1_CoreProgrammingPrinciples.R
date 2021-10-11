353371775c6e443bcc2db07a16823bbdb82bfd4f
Soha Pourpirali <spourpirali@newrelic.com>
2021-10-11 08:42:33 -0700

    review of using variables in basic operations

--- L1_CoreProgrammingPrinciples.R
+++ L1_CoreProgrammingPrinciples.R
@@ -1,3 +1,5 @@
+##section2: core programming principles
+
 #integer (the "L" makes the value an integer. Without it, R stores the variable as "double.")
 x <- 2L
 typeof(x)
@@ -17,3 +19,51 @@ typeof(a)
 #logical 
 q <- TRUE
 typeof(q)
+
+#using variables
+A <- 10
+B <- 5
+C <- A+B
+C
+
+var1 <- 2.5
+var2 <- 4
+
+result <- var1 / var2
+result
+
+answer <- sqrt(var2)
+answer
+
+greeting <- "hello"
+name <- "bob"
+message <- paste(greeting, name)
+message
+
+#logical 
+
+4 < 5 
+10 > 100
+4 == 5 
+4 != 5
+4 >= 5
+
+result <- 4 < 5
+result
+
+result2 <- !(5 > 1)
+result2
+
+# | is the or operator. It will come back true if either side of the operation is true. 
+
+4 == 4 | 4 == 5 
+result | result2
+
+# & is the and operator. It will come back true only if both sides of the operation are true. 
+
+4 == 4 & 4 == 5 
+result & result2
+
+# isTRUE() just tells you if the thing inside the parentheses is true or not. 
+
+isTRUE(4 == 4)
