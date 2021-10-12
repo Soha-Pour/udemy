01fc0f91dcb96658a805442b0afbc93c6ed1f3cf
Soha Pourpirali <spourpirali@newrelic.com>
2021-10-12 09:30:00 -0700

    while loop, for loop, if statement

--- SECTION2.R
+++ SECTION2.R
@@ -30,6 +30,7 @@ result
 greeting <- "hello"
 name <- "soha"
 message <- paste(greeting, name)
+message
 
 #LOGICAL VARIABLES AND OPERATORS
 
@@ -57,3 +58,63 @@ while(number < 1000){
   print(number)
   number <- number * 2
 }
+
+#THE FOR LOOP
+#doesn't have to be "i" it can be anything.
+#for (val in sequence)
+#{
+#  statement
+#}
+# Sequence is a vector and val takes on each of its values during the loop. In each iteration, statement is evaluated. 
+
+for(i in 1:5){
+  print("Hello R")
+}
+
+for(i in 1:10){
+  print(i * 2)
+}
+
+#counts even numbers
+x <- c(2,5,3,9,8,11,6)
+count <- 0
+for (val in x) {
+  if(val %% 2 == 0)  count = count+1
+}
+print(count)
+
+#counts odd numbers
+x <- c(2,5,3,9,8,11,6)
+count <- 0
+for (val in x) {
+  if(val %% 2 != 0)  count = count+1
+}
+print(count)
+
+#THE IF STATEMENT
+
+#rnorm() generates a set of numbers which are randomly distributed according to the normal distribution. 
+#falls between -2 and 2.
+#rnorm(1) generates exactly one number. 
+#rm() removes a variable. we do this so that r doesn't store the answer variable from the last time the code is executed.
+#else allows you to specify what happens if the if statement is not true. 
+
+rm(answer)
+x <- rnorm(1)
+if(x > 1){
+  answer <- "Greater than 1"
+} else{
+  answer <- "Less than or equal to 1"
+}
+
+rm(answer)
+x <- rnorm(1)
+if(x > 1){
+  answer <- "Greater than 1"
+} else if(x >= -1){
+  answer <- "Between -1 and 1"
+} else{
+  answer <- "Less than -1"
+}
+
+
